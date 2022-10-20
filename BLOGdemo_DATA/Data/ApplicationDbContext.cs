8607deec96c7e83ffdcece9686cc6f2feff372ca
Matthew Carapella <matthew.m.carapella@gmail.com>
2022-10-20 17:16:45 -0400

    Add project files.

---
+++ BLOGdemo_DATA/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BLOGdemo_DATA.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+}
